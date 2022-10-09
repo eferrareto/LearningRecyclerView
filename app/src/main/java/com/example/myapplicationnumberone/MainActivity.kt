d6c24ea14c40d3f9f2bd5e83dd4d81053f00dc17
Erick Ferrareto <e.ferraretowork@gmail.com>
2022-10-08 22:38:42 -0300

    in progress

--- app/src/main/java/com/example/myapplicationnumberone/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationnumberone/MainActivity.kt
@@ -2,47 +2,100 @@ package com.example.myapplicationnumberone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Adapter
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplicationnumberone.model.Categoria
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recycler :RecyclerView
+    private lateinit var rcView : RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        recycler = findViewById(R.id.rcview)
-        var adapter = MainAdapter()
-        recycler.adapter = adapter
-        recycler.layoutManager = LinearLayoutManager(this)
+        val lista = mutableListOf<Categoria>()
+
+        rcView = findViewById(R.id.rcView)
+        rcView.adapter = MainAdapter(lista)
+        rcView.layoutManager = LinearLayoutManager(this)
+
+
+        lista.add(
+            Categoria(
+                title = "Olá Mundo",
+                name = "Erick Ferrareto"
+            )
+        )
+        lista.add(
+            Categoria(
+                title = "My World",
+                name = "Tayna Nogueira"
+            )
+        )
+        lista.add(
+            Categoria(
+                title = "My Brother",
+                name = "Pedro Henrique"
+            )
+        )
+        lista.add(
+            Categoria(
+                title = "My Mother",
+                name = "Alessandra Ferrareto"
+            )
+        )
+        lista.add(
+            Categoria(
+                title = "My father",
+                name = "Haroldo Ferrareto"
+            )
+        )
+        lista.add(
+            Categoria(
+                title = "My Grandmother",
+                name = "Neide Souza"
+            )
+        )
+        lista.add(
+            Categoria(
+                title = "My Grandfather",
+                name = "Valdemar Pereira"
+            )
+        )
+
+
 
 
     }
-    private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>() {
+    private inner class MainAdapter(var lista : List<Categoria>) : RecyclerView.Adapter<MainViewHolder>(){
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
-            var view = layoutInflater.inflate(R.layout.layout_rv, parent,false)
-            var viewHolder = MainViewHolder(view)
-            return viewHolder
+            val view = layoutInflater.inflate(R.layout.layout_rv, parent, false)
+            return MainViewHolder(view)
         }
 
         override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
-
+            val itemCurrent = lista[position]
+            holder.bind(itemCurrent)
         }
 
         override fun getItemCount(): Int {
-            return 30
+           return lista.size
         }
+
     }
+    private inner class MainViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        fun bind(item: Categoria){
 
-
-    private class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-
+            val txtTitle : TextView = findViewById(R.id.txtTitle)
+            txtTitle.text = item.title
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplicationnumberone/model/Categoria.kt
@@ -0,0 +1,6 @@
+package com.example.myapplicationnumberone.model
+
+data class Categoria(
+    var title : String,
+    var name : String
+)
